80cab5bb58d614d3f09945ef775255d4a16d28a8
Freddie Haddad <freddie.haddad@outlook.com>
2023-11-16 13:21:20 -0800

    feat(repl): Initial REPL support.
    This version just prints the tokens.  As more functionality is built in,
    the REPL will be expanded eventually doing full execution.

--- cmd/corrosion/corrosion.go
+++ cmd/corrosion/corrosion.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/freddiehaddad/corrosion/pkg/lexer"
+	"github.com/freddiehaddad/corrosion/pkg/token"
+)
 
 const appName = "Corrosion"
+const prompt = "> "
 
 func main() {
-	fmt.Println("Welcome to ", appName)
+	fmt.Println("Welcome to", appName)
+	fmt.Println("Press Ctrl+D (^D) to exit")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(prompt)
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		l := lexer.New(input)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+
+		fmt.Print(prompt)
+	}
+
+	fmt.Println("Exiting", appName)
 }
--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -8,10 +8,10 @@ import (
 
 func compareToken(t *testing.T, i int, et, nt token.Token) {
 	if nt.Type != et.Type {
-		t.Fatalf("tests[%d] - token type wrong: expected=%q got=%q", i, et.Type, nt.Type)
+		t.Errorf("tests[%d] - token type wrong: expected=%q got=%q", i, et.Type, nt.Type)
 	}
 	if nt.Literal != et.Literal {
-		t.Fatalf("tests[%d] - token literal wrong: expected=%q got=%q", i, et.Literal, nt.Literal)
+		t.Errorf("tests[%d] - token literal wrong: expected=%q got=%q", i, et.Literal, nt.Literal)
 	}
 }
 
@@ -142,10 +142,10 @@ func TestUnknownInput(t *testing.T) {
 func TestMultiDigitTokens(t *testing.T) {
 	input := `==!=&&||`
 	tests := []token.Token{
-		{Type: token.EQUAL, Literal: token.EQUAL},
-		{Type: token.NOT_EQUAL, Literal: token.NOT_EQUAL},
-		{Type: token.AND, Literal: token.AND},
-		{Type: token.OR, Literal: token.OR},
+		{Type: token.EQUAL, Literal: "=="},
+		{Type: token.NOT_EQUAL, Literal: "!="},
+		{Type: token.AND, Literal: "&&"},
+		{Type: token.OR, Literal: "||"},
 	}
 
 	l := New(input)
@@ -155,26 +155,26 @@ func TestMultiDigitTokens(t *testing.T) {
 func TestSingleCharaterTokens(t *testing.T) {
 	input := `;,()[]{}=+-*/<>&|^~!`
 	tests := []token.Token{
-		{Type: token.SEMICOLON, Literal: token.SEMICOLON},
-		{Type: token.COMMA, Literal: token.COMMA},
-		{Type: token.LPAREN, Literal: token.LPAREN},
-		{Type: token.RPAREN, Literal: token.RPAREN},
-		{Type: token.LBRACKET, Literal: token.LBRACKET},
-		{Type: token.RBRACKET, Literal: token.RBRACKET},
-		{Type: token.LBRACE, Literal: token.LBRACE},
-		{Type: token.RBRACE, Literal: token.RBRACE},
-		{Type: token.ASSIGN, Literal: token.ASSIGN},
-		{Type: token.PLUS, Literal: token.PLUS},
-		{Type: token.MINUS, Literal: token.MINUS},
-		{Type: token.ASTERISK, Literal: token.ASTERISK},
-		{Type: token.FSLASH, Literal: token.FSLASH},
-		{Type: token.LESS_THAN, Literal: token.LESS_THAN},
-		{Type: token.GREATER_THAN, Literal: token.GREATER_THAN},
-		{Type: token.BITWISE_AND, Literal: token.BITWISE_AND},
-		{Type: token.BITWISE_OR, Literal: token.BITWISE_OR},
-		{Type: token.BITWISE_XOR, Literal: token.BITWISE_XOR},
-		{Type: token.BITWISE_NOT, Literal: token.BITWISE_NOT},
-		{Type: token.BANG, Literal: token.BANG},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.LBRACKET, Literal: "["},
+		{Type: token.RBRACKET, Literal: "]"},
+		{Type: token.LBRACE, Literal: "{"},
+		{Type: token.RBRACE, Literal: "}"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.PLUS, Literal: "+"},
+		{Type: token.MINUS, Literal: "-"},
+		{Type: token.ASTERISK, Literal: "*"},
+		{Type: token.FSLASH, Literal: "/"},
+		{Type: token.LESS_THAN, Literal: "<"},
+		{Type: token.GREATER_THAN, Literal: ">"},
+		{Type: token.BITWISE_AND, Literal: "&"},
+		{Type: token.BITWISE_OR, Literal: "|"},
+		{Type: token.BITWISE_XOR, Literal: "^"},
+		{Type: token.BITWISE_NOT, Literal: "~"},
+		{Type: token.BANG, Literal: "!"},
 		{Type: token.EOF, Literal: string(token.EOF_VALUE)},
 	}
 	l := New(input)
